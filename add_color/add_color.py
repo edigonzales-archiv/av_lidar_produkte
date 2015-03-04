5f14a8c5793b49dd34754899b4eee30c8d6e493a
Stefan Ziegler <edi.gonzales@gmail.com>
2015-03-04 21:03:37 +0100

    angepasst an 14.04 und home nas

--- add_color/add_color.py
+++ add_color/add_color.py
@@ -9,10 +9,14 @@ import ntpath
 
 ogr.UseExceptions()
 
-LIDAR_PATH = "/opt/Geodaten/ch/so/kva/hoehen/2014/lidar/"
-ORTHO_SHP = "/opt/Geodaten/ch/so/kva/orthofoto/2014/rgb/12_5cm/ortho2014.shp"
-TMP_PATH = "/home/stefan/tmp/color/"
-BUFFER = 50
+#LIDAR_PATH = "/opt/Geodaten/ch/so/kva/hoehen/2014/lidar/"
+#ORTHO_SHP = "/opt/Geodaten/ch/so/kva/orthofoto/2014/rgb/12_5cm/ortho2014.shp"
+#TMP_PATH = "/home/stefan/tmp/color/"
+LIDAR_PATH = "/home/stefan/mr_candie_nas/Geodaten/ch/so/agi/hoehen/2014/lidar/"
+ORTHO_SHP = "/home/stefan/mr_candie_nas/Geodaten/ch/so/agi/orthofoto/2014/rgb/12_5cm/ortho2014.shp"
+ORTHO_PATH = "/home/stefan/mr_candie_nas/Geodaten/ch/so/agi/orthofoto/2014/rgb/12_5cm/"
+TARGET_PATH = "/home/stefan/mr_candie_nas/Geodaten/ch/so/agi/hoehen/2014/lidar/color/"
+TMP_PATH = "/home/stefan/tmp/"
 
 shp = ogr.Open(ORTHO_SHP)
 layer = shp.GetLayer(0)
@@ -34,28 +38,21 @@ for feature in layer:
     
     outfile_las = os.path.join(TMP_PATH, "LAS_" + base_name + ".las")
     cmd = "laszip.exe -i " + laz_file_name + " -o " + outfile_las
-    os.system(cmd)
-    
-    outfile_color = os.path.join(TMP_PATH, "LAS_" + base_name + "_color.las")
-    cmd = "las2las -i " + outfile_las + " --color-source " + ortho_file_name + " --file-format 1.2 --point-format 3 -o " + outfile_color
-    os.system(cmd)
+    #os.system(cmd)
+    #print cmd
 
-    outfile_color_laz = os.path.join(TMP_PATH, "LAS_" + base_name + ".laz")
+    ortho_file_name = os.path.join(ORTHO_PATH, base_name + "_12_5cm.tif")
+    outfile_color = os.path.join(TMP_PATH, "LAS_color.las")
+    cmd = "las2las -i " + laz_file_name + " --color-source " + ortho_file_name + " --file-format 1.2 --point-format 3 -o " + outfile_color
+    os.system(cmd)
+    #print cmd
+
+    outfile_color_laz = os.path.join(TARGET_PATH, "LAS_" + base_name + ".laz")
     cmd = "laszip.exe -i " + outfile_color + " -o " + outfile_color_laz
     os.system(cmd)
-
-    print cmd
+    #print cmd
     
     
-    break
+    #break
     
-    geom = feature.GetGeometryRef()
-    env = geom.GetEnvelope()
-
-    minX = int(env[0] + 0.001)
-    minY = int(env[2] + 0.001)
-    maxX = int(env[1] + 0.001)
-    maxY = int(env[3] + 0.001)
-    
-    outfile = os.path.join(TMP_PATH, "input.tif")
     
